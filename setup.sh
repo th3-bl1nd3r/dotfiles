c264db1ee8bacfd856c1971d46a45b92be1fee14
th3-bl1nd3r <gianghia056@gmail.com>
2023-11-18 08:46:59 +0700

    zsh setup

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install zsh -y
+sudo apt install git -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
